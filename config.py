c738cd0f48ac61df8aaf500c713f7074cf22a733
Divyansh Kapoor <72908369+divyansh0043@users.noreply.github.com>
2024-02-07 15:37:35 +0530

    integerated openai api in voice assistant

--- config.py
+++ config.py
@@ -1,3 +1,2 @@
 # todo: Add your api key here
 
-apikey = "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"
--- main.py
+++ main.py
@@ -1,10 +1,30 @@
-# import openai
+import openai
 from openai import OpenAI
 import speech_recognition as sr
 from config import apikey
 import os
 import webbrowser
-import random
+chatStr = ""
+
+def chat(query):
+    global chatStr
+    print(chatStr)
+    client = OpenAI(api_key=apikey)
+    chatStr += f"DK: {query}\nAI: "
+    response = client.completions.create(
+        model="gpt-3.5-turbo-instruct",
+        prompt=chatStr,
+        temperature=0.7,
+        max_tokens=256,
+        top_p=1,
+        frequency_penalty=0,
+        presence_penalty=0
+    )
+    # todo: wrap inside a try catch block
+
+    chatStr += f"{response.choices[0].text}\n"
+    say(response.choices[0].text)
+    return response.choices[0].text
 
 
 def ai(prompt):
@@ -13,15 +33,17 @@ def ai(prompt):
     response = client.completions.create(
         model="gpt-3.5-turbo-instruct",
         prompt=prompt,
-        temperature=1,
+        temperature=0.7,
         max_tokens=256,
         top_p=1,
         frequency_penalty=0,
         presence_penalty=0
     )
     # todo: wrap inside a try catch block
-    print(response["choices"][0]["text"])
-    text += response["choices"][0]["text"]
+
+    text += response.choices[0].text
+
+
     if not os.path.exists("Openai"):
         os.mkdir("Openai")
 
@@ -29,12 +51,9 @@ def ai(prompt):
         f.write(text)
 
 
+
 def say(text):
     os.system(f"say {text}")
-    print(text)
-
-
-
 
 def input():
     r = sr.Recognizer()
@@ -42,28 +61,34 @@ def input():
         # r.pause_threshold = 1
         audio = r.listen(source)
         try:
-            query = r.recognize_google_cloud(audio, language="en-in")
+            query = r.recognize_google(audio, language="en-in")
             print(f"User said:{query}")
             print(query)
             return query
         except Exception as e:
-            return "Some Error Occured, Sorry"
+            return "Some Error occured, Sorry"
 if __name__ == '__main__':
     print('PyCharm')
     say("Hello i m dk")
     while True:
         print("Listening...")
         query = input()
-        # sites = [["youtube", "https://youtube.com"], ["wikipedia", "https://wikipedia.com"], ["google", "https://google.com"],]
-        # for site in sites:
-        #     if f"Open {site[0]}".lower() in query.lower():
-        #         say(f"Opening {site[0]} sir")
-        #         webbrowser.open(site[1])
+        sites = [["youtube", "https://youtube.com"], ["wikipedia", "https://wikipedia.com"], ["google", "https://google.com"],]
+        for site in sites:
+            if f"Open {site[0]}".lower() in query.lower():
+                say(f"Opening {site[0]} sir")
+                webbrowser.open(site[1])
         # say(query)
         if f"what are you doing".lower() in query.lower():
             say("just chilling sir")
 
-        # if "Using artificial intelligence".lower() in query.lower():
-        #     ai(prompt=query)
+        elif "Using artificial intelligence".lower() in query.lower():
+            ai(prompt=query)
+        elif "AI Quit".lower() in query.lower():
+            exit()
 
-
+        elif "reset chat".lower() in query.lower():
+            chatStr = ""
+        else:
+            print("Chatting...")
+            chat(query)
